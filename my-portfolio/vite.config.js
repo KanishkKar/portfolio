39f658bfa4d14d95b5973bee1d95a40d1653fcaf
Kanishk Kar <80909280+KanishkKar@users.noreply.github.com>
2022-07-24 22:29:21 +0530

    run build

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./docs",
+  },
+  base: "/portfolio/",
   plugins: [vue()]
 })
